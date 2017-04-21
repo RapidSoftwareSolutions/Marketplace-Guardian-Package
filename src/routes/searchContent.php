5d3d98b9d2d09a26f9c80a5e372f94563ad9c94d
ivz-dev <zakutynsky@yahoo.com>
2017-04-21 10:56:53 +0300

    fixed marketplace bug

--- src/routes/searchContent.php
+++ src/routes/searchContent.php
@@ -18,7 +18,7 @@ $app->post('/api/Guardian/searchContent', function ($request, $response) {
 
     foreach ($post_data['args'] as $key=>$item)
     {
-        if(in_array($key, $params))
+        if(in_array($key, $params) && !empty($key))
         {
             if($key=='query'){
                 $data['q']=$item;
